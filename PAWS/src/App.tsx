aad0929cbcf2d64de7e30ce3e5e6aaf55c8e4f31
Arvind Anthey <122073391+aanthey@users.noreply.github.com>
2025-10-03 21:29:07 +0000

    Created a template navbar

--- PAWS/src/App.tsx
+++ PAWS/src/App.tsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 import './App.css'
-import './components/post/post.tsx'
+
+import Navbar from './components/navbar'
 import Post from './components/post/post.tsx'
 
 function App() {
 
   return (
     <>
+      <Navbar />
       <Post 
         user="jimmy_chang"
         imageUrl='https://images.unsplash.com/photo-1552053831-71594a27632d?q=80&w=662&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
         caption="Hello, this is my caption"
         timestamp={10032025}
-        />
+      />
     </>
   )
 }
