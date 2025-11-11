41218f4f84cc7cebe7b7d05b6254522c5f4d4520
Xhesikam <xhesikam2184@tywls.org>
2025-11-11 20:04:14 +0000

    finished profile page

--- paws-next/src/components/profile/profileposts.tsx
+++ paws-next/src/components/profile/profileposts.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 function ProfilePost() {
   return (
     <div>
-        <img src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"  width="84" height="84"/>
+        <img src="https://thumbs.dreamstime.com/b/cute-cat-portrait-square-photo-beautiful-white-closeup-105311158.jpg"
+        width="250" height="250"/>
     </div>
   );
 }
